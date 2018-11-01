417365785ecb581f8c900d802a51da4b1dfd62bb
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2018-11-01 14:51:55 -0300

    Aula 01

---
+++ Aula 01.py
@@ -0,0 +1,24 @@
+# Aula 01
+# Revisão de conceitos e Aprimoramento.
+print("Revisão de Python")
+
+# Teste de interação com o python
+print(7+4)
+print('7'+'4')
+print('Olá',10)
+
+
+# Variaveis
+nome = "ACO"
+idade = int(25)
+peso = float(75.5)
+print(nome,idade,peso)
+
+
+print()
+print()
+print("Inserção de informações: ")
+nome = input("Qual seu nome: ")
+idade = int(input("Qual sua idade:"))
+peso = float(input("Qual seu peso:"))
+print(nome,idade,peso)
---
+++ Exe - 1.py
@@ -0,0 +1,4 @@
+print("Desafio")
+nome = input("Por favor digitie seu nome: ")
+
+print("Olá",nome,"Prazer em te conhecer!")
