c7d67d549b481b1f7f2ff3368bb9ab8e5e4aff4c
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2018-11-06 13:48:59 -0200

    Criação dos exercícios

---
+++ Python Exercícios/Ex 014.py
@@ -0,0 +1,5 @@
+#14 Conversor de temperatus Python
+# Criação do algoritmo para converter temperaturas
+
+temp= float(input("Informar temperatura em ºC: "))
+print("Temperatura em Fahrenheit: {}ºF".format(((9 * temp) / 5) + 32))
